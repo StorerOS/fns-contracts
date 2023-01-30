e0a3367191158ffc219bd3955bd5a0a747d7ea14
libin <libin@wenchu.io>
2023-01-30 16:35:17 +0800

    added public resolver

--- FnsDeployer.sol
+++ FnsDeployer.sol
@@ -5,7 +5,9 @@ import "./ensdomains/ens-contracts/contracts/ethregistrar/StablePriceOracle.sol"
 import "./ensdomains/ens-contracts/contracts/ethregistrar/BaseRegistrarImplementation.sol";
 import "./ensdomains/ens-contracts/contracts/ethregistrar/StringUtils.sol";
 import "./ensdomains/ens-contracts/contracts/resolvers/Resolver.sol";
-import './ensdomains/ens-contracts/contracts/registry/ENSRegistry.sol';
+import "./ensdomains/ens-contracts/contracts/resolvers/profiles/AddrResolver.sol";
+import "./ensdomains/ens-contracts/contracts/registry/ENSRegistry.sol";
+import "./ensdomains/ens-contracts/contracts/resolvers/OwnedResolver.sol";
 
 contract MyOracle {
     int256 value;
@@ -29,19 +31,19 @@ contract MyOracle {
  */
 contract FnsDeployer is Ownable {
     using StringUtils for *;
-    uint[] rentPrices = new uint[](6); //租赁定价系数(域名越短，系数越大)
+    uint[] private rentPrices = new uint[](6); //租赁定价系数(域名越短，系数越大)
 
-    string private constant  LTD_NAME = 'fil';
-    string private constant  RESOLVER_NAME = 'resolver';
-    string private constant  REVERSE_NAME = 'reverse';
-    string private constant  ADDR_NAME = 'addr';
-    bytes32 public constant  TLD_LABEL = keccak256(bytes(LTD_NAME));
-    bytes32 public constant  RESOLVER_LABEL = keccak256(bytes(RESOLVER_NAME));
-    bytes32 public constant  REVERSE_REGISTRAR_LABEL = keccak256(bytes(REVERSE_NAME));
-    bytes32 public constant  ADDR_LABEL = keccak256(bytes(ADDR_NAME));
-    uint constant public MIN_REGISTRATION_DURATION = 28 days;
+    string private constant   LTD_NAME = 'fil';
+    string private constant   RESOLVER_NAME = 'resolver';
+    string private constant   REVERSE_NAME = 'reverse';
+    string private constant   ADDR_NAME = 'addr';
+    bytes32 private constant  TLD_LABEL = keccak256(bytes(LTD_NAME));
+    bytes32 private constant  RESOLVER_LABEL = keccak256(bytes(RESOLVER_NAME));
+    bytes32 private constant  REVERSE_REGISTRAR_LABEL = keccak256(bytes(REVERSE_NAME));
+    bytes32 private constant  ADDR_LABEL = keccak256(bytes(ADDR_NAME));
+    uint constant private     MIN_REGISTRATION_DURATION = 28 days;
 
-bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(bytes4)"));
+    bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(bytes4)"));
     bytes4 constant private COMMITMENT_CONTROLLER_ID = bytes4(
         keccak256("rentPrice(string,uint256)") ^
         keccak256("available(string)") ^
@@ -59,27 +61,29 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
     bytes32 private baseNode; //根节点
     MyOracle private oracle; //价格生成器
     ENSRegistry private ens; //ENS注册器
-    BaseRegistrarImplementation base; //基本注册器实现
-    PriceOracle prices; //定价预言机
-    uint public minCommitmentAge;
-    uint public maxCommitmentAge;
+    BaseRegistrarImplementation private  base; //基本注册器实现
+    PriceOracle private prices; //定价预言机
+    OwnedResolver private publicResolver;
+    uint private minCommitmentAge;
+    uint private maxCommitmentAge;
 
-    mapping(bytes32=>uint) public commitments;
+    mapping(bytes32 => uint) public commitments;
 
-    event NameRegistered(string name, bytes32 indexed label, address indexed owner, uint cost, uint expires);
-    event NameRenewed(string name, bytes32 indexed label, uint cost, uint expires);
-    event NewPriceOracle(address indexed oracle);
+    event FnsNameRegistered(string name, bytes32 indexed label, address indexed owner, uint cost, uint expires);
+    event FnsNameRenewed(string name, bytes32 indexed label, uint cost, uint expires);
+    event FnsNewPriceOracle(address indexed oracle);
 
     constructor() public {
         minCommitmentAge = 0;
         maxCommitmentAge = 0;
-        rentPrices=[0]; //定价系数
+        rentPrices = [10000000, 100000, 10000, 1000, 100, 0]; //定价系数
         oracle = new MyOracle(7);
         prices = new StablePriceOracle(AggregatorInterface(address(oracle)), rentPrices);
         baseNode = namehash(bytes32(0), TLD_LABEL);
         ens = new ENSRegistry();
+        publicResolver = new OwnedResolver();
         base = new BaseRegistrarImplementation(ens, baseNode);
-        ens.setSubnodeRecord(bytes32(0), TLD_LABEL, address(base), address(0), 0);
+        ens.setSubnodeRecord(bytes32(0), TLD_LABEL, address(base), address(publicResolver), 0);
         base.addController(address(this));
     }
 
@@ -91,33 +95,49 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
         return keccak256(bytes(name));
     }
 
-    function rentPrice(string memory name, uint duration) view public returns(uint) {
+    function rentPrice(string memory name, uint duration) view public returns (uint) {
         bytes32 hash = keccak256(bytes(name));
         return prices.price(name, base.nameExpires(uint256(hash)), duration);
     }
 
-    function valid(string memory name) public pure returns(bool) {
+    function valid(string memory name) public pure returns (bool) {
         return name.strlen() >= 3;
     }
 
-    function available(string memory name) public view returns(bool) {
+    function available(string memory name) public view returns (bool) {
         bytes32 label = keccak256(bytes(name));
         return valid(name) && base.available(uint256(label));
     }
 
-    function addController(address addr) public {
-        return base.addController(addr);
+    function addr(string memory name) public view returns (address) {
+        bytes32 label = keccak256(bytes(name));
+        bytes32 node = namehash(baseNode, label);
+        address resolver = ens.resolver(node);
+        if(resolver == address(0)) {
+            return address(0);
+        }
+        return Resolver(resolver).addr(node);
     }
 
-    function removeController(address addr) public {
-        base.removeController(addr);
+    function ownerOf(string memory name) public view returns (address) {
+        bytes32 label = keccak256(bytes(name));
+        bytes32 node = namehash(baseNode, label);
+        return ens.owner(node);
     }
 
-    function makeCommitment(string memory name, address owner, bytes32 secret) pure public returns(bytes32) {
+    function resolver(string memory name) public view returns (address) {
+        bytes32 label = keccak256(bytes(name));
+        bytes32 node = namehash(baseNode, label);
+        return ens.resolver(node);
+    }
+
+    /*---------------------------------------------------------------------------------------------------------------*/
+
+    function makeCommitment(string memory name, address owner, bytes32 secret) pure public returns (bytes32) {
         return makeCommitmentWithConfig(name, owner, secret, address(0), address(0));
     }
 
-    function makeCommitmentWithConfig(string memory name, address owner, bytes32 secret, address resolver, address addr) pure public returns(bytes32) {
+    function makeCommitmentWithConfig(string memory name, address owner, bytes32 secret, address resolver, address addr) pure public returns (bytes32) {
         bytes32 label = keccak256(bytes(name));
         if (resolver == address(0) && addr == address(0)) {
             return keccak256(abi.encodePacked(label, owner, secret));
@@ -131,11 +151,30 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
         commitments[commitment] = block.timestamp;
     }
 
-    function register(string calldata name, address owner, uint duration, bytes32 secret) external payable {
-        registerWithConfig(name, owner, duration, secret, address(0), address(0));
+    function setResolver(string memory name, address resolver) public {
+        bytes32 label = keccak256(bytes(name));
+        bytes32 node = namehash(baseNode, label);
+        address owner = ens.owner(node);
+        require(owner == msg.sender, "msg sender is not domain owner");
+        require(resolver != address(0), "name resolver is empty");
+        ens.setResolver(node, resolver);
     }
 
-//    event FnsLog(string log);
+    function setAddr(string memory name, address addr) public {
+        bytes32 label = keccak256(bytes(name));
+        bytes32 node = namehash(baseNode, label);
+        address owner = ens.owner(node);
+        require(owner == msg.sender, "msg sender is not domain owner");
+        address resolver = ens.resolver(node);
+        require(resolver != address(0), "name resolver is empty");
+        Resolver(resolver).setAddr(node, addr);
+    }
+
+    function register(string calldata name, address owner, uint duration, bytes32 secret) external payable {
+        registerWithConfig(name, owner, duration, secret, address(publicResolver), owner);
+    }
+
+    //    event FnsLog(string log);
     function registerWithConfig(string memory name, address owner, uint duration, bytes32 secret, address resolver, address addr) public payable {
         bytes32 commitment = makeCommitmentWithConfig(name, owner, secret, resolver, addr);
         uint cost = _consumeCommitment(name, duration, commitment);
@@ -144,7 +183,7 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
         uint256 tokenId = uint256(label);
 
         uint expires;
-        if(resolver != address(0)) {
+        if (resolver != address(0)) {
             // Set this contract as the (temporary) owner, giving it
             // permission to set up the resolver.
             expires = base.register(tokenId, address(this), duration);
@@ -164,14 +203,12 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
             base.reclaim(tokenId, owner);
             base.transferFrom(address(this), owner, tokenId);
         } else {
-             require(addr == address(0), "address must be 0");
-             expires = base.register(tokenId, owner, duration);
+            require(addr == address(0), "address must be 0");
+            expires = base.register(tokenId, owner, duration);
         }
-
-        emit NameRegistered(name, label, owner, cost, expires);
-
+        emit FnsNameRegistered(name, label, owner, cost, expires);
         //Refund any extra payment
-        if(msg.value > cost) {
+        if (msg.value > cost) {
             payable(msg.sender).transfer(msg.value - cost);
         }
     }
@@ -183,16 +220,16 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
         bytes32 label = keccak256(bytes(name));
         uint expires = base.renew(uint256(label), duration);
 
-        if(msg.value > cost) {
+        if (msg.value > cost) {
             payable(msg.sender).transfer(msg.value - cost);
         }
 
-        emit NameRenewed(name, label, cost, expires);
+        emit FnsNameRenewed(name, label, cost, expires);
     }
 
     function setPriceOracle(PriceOracle _prices) public onlyOwner {
         prices = _prices;
-        emit NewPriceOracle(address(prices));
+        emit FnsNewPriceOracle(address(prices));
     }
 
     function setCommitmentAges(uint _minCommitmentAge, uint _maxCommitmentAge) public onlyOwner {
@@ -212,13 +249,13 @@ bytes4 constant private INTERFACE_META_ID = bytes4(keccak256("supportsInterface(
 
     function _consumeCommitment(string memory name, uint duration, bytes32 commitment) internal returns (uint256) {
         // Require a valid commitment
-       // require(commitments[commitment] + minCommitmentAge <= block.timestamp, "min commit age not valid");
+        // require(commitments[commitment] + minCommitmentAge <= block.timestamp, "min commit age not valid");
 
         // If the commitment is too old, or the name is registered, stop
         //require(commitments[commitment] + maxCommitmentAge > block.timestamp, "max commit age not valid");
         require(available(name), "name is registered");
 
-        delete(commitments[commitment]);
+        delete (commitments[commitment]);
 
         uint cost = rentPrice(name, duration);
         require(duration >= MIN_REGISTRATION_DURATION, "duration is less than min registration duration");
